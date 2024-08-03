2ce5b22cf91957403a160c11386098b3cd66bc79
kazukishirasu <s19c1057fa@s.chibakoudai.jp>
2024-08-03 18:23:54 +0900

    initial commit

---
+++ include/map_changer/map_changer_node.h
@@ -0,0 +1,36 @@
+#ifndef MAP_CHANGER_NODE_H__
+#define MAP_CHANGER_NODE_H__
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <string>
+#include <vector>
+#include <yaml-cpp/yaml.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <waypoint_manager_msgs/Waypoint.h>
+#include <nav_msgs/LoadMap.h>
+
+namespace map_changer {
+
+class map_changer_node
+{
+public:
+    map_changer_node();
+    ~map_changer_node();
+    void cb_wp(const waypoint_manager_msgs::Waypoint::ConstPtr &msg);
+    void read_yaml();
+    void send_map(int);
+    void send_pose(geometry_msgs::Pose);
+private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Subscriber wp_sub_;
+    ros::ServiceClient map_srv_;
+    ros::Publisher pose_pub_;
+    std::string file_path_;
+    std::vector<std::array<std::string, 2>> config_list;
+};
+
+}
+#endif
---
+++ src/map_changer_node.cpp
@@ -0,0 +1,76 @@
+#include "map_changer/map_changer_node.h"
+
+namespace map_changer {
+
+map_changer_node::map_changer_node() : pnh_("~")
+{
+    ROS_INFO("Start map_changer_node");
+    read_yaml();
+    wp_sub_ = nh_.subscribe("/waypoint_manager/waypoint", 1, &map_changer_node::cb_wp, this);
+    map_srv_ = nh_.serviceClient<nav_msgs::LoadMap>("/change_map");
+    pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1, this);
+}
+
+map_changer_node::~map_changer_node()
+{
+}
+
+void map_changer_node::cb_wp(const waypoint_manager_msgs::Waypoint::ConstPtr &msg)
+{
+    static int index = 0;
+    static std::string old_id;
+    if (config_list.size() - 1 >= index)
+    {
+        std::string id = config_list[index][0];
+        if (msg->identity == id && msg->identity != old_id)
+        {
+            send_map(index);
+            send_pose(msg->pose);
+            index++;
+        }
+    }
+    old_id = msg->identity;
+}
+
+void map_changer_node::read_yaml()
+{
+    pnh_.param("file_path", file_path_, std::string(ros::package::getPath("map_changer") += "/config/test.yaml"));
+    ROS_INFO("Load %s", file_path_.c_str());
+    YAML::Node config = YAML::LoadFile(file_path_);
+    for (const auto& node:config["config"])
+    {
+        std::array<std::string, 2> tmp;
+        tmp[0] = node["waypoint_id"].as<std::string>();
+        tmp[1] = node["map_file"].as<std::string>();
+        config_list.push_back(tmp);
+    }
+}
+
+void map_changer_node::send_map(int index)
+{
+    nav_msgs::LoadMap srv;
+    srv.request.map_url = config_list[index][1];
+    if (map_srv_.call(srv))
+    {
+        ROS_INFO("Map change to %s", srv.request.map_url.c_str());
+    } else {
+        ROS_ERROR("Failed to call service /change_map");
+    }
+}
+
+void map_changer_node::send_pose(geometry_msgs::Pose pose)
+{
+    geometry_msgs::PoseWithCovarianceStamped msg;
+    msg.pose.pose = pose;
+    pose_pub_.publish(msg);
+    ROS_INFO("Teleport to x:%lf, y:%lf", msg.pose.pose.position.x, msg.pose.pose.position.y);
+}
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "map_changer_node");
+    map_changer::map_changer_node mc;
+    ros::spin();
+    return 0;
+}
