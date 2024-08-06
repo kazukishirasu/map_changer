5e391bfeaa4e409dc6d4a8e1370435cc97be1f2e
kazukishirasu <s19c1057fa@s.chibakoudai.jp>
2024-08-06 17:08:43 +0900

    update

--- include/map_changer/map_changer_node.h
+++ include/map_changer/map_changer_node.h
@@ -10,7 +10,9 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <tf/transform_broadcaster.h>
 #include <waypoint_manager_msgs/Waypoint.h>
+#include <std_msgs/Bool.h>
 #include <nav_msgs/LoadMap.h>
+#include <std_srvs/Trigger.h>
 
 namespace map_changer {
 
@@ -20,18 +22,24 @@ public:
     map_changer_node();
     ~map_changer_node();
     void cb_wp(const waypoint_manager_msgs::Waypoint::ConstPtr &msg);
+    void cb_reach(std_msgs::Bool msg);
     void read_yaml();
+    void call_next_wp();
     void send_map(int);
-    void send_pose(geometry_msgs::Pose);
+    void send_emclpose(geometry_msgs::Pose);
+    void send_initialpose(geometry_msgs::Pose);
 private:
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
     ros::Subscriber wp_sub_;
+    ros::Subscriber reach_sub_;
+    ros::ServiceClient next_wp_srv_;
     ros::ServiceClient map_srv_;
     ros::ServiceClient costmap_srv_;
     ros::Publisher pose_pub_;
     std::string file_path_;
     std::vector<std::array<std::string, 2>> config_list;
+    bool reach_flag = false;
 };
 
 }
--- src/map_changer_node.cpp
+++ src/map_changer_node.cpp
@@ -7,6 +7,8 @@ map_changer_node::map_changer_node() : pnh_("~")
     ROS_INFO("Start map_changer_node");
     read_yaml();
     wp_sub_ = nh_.subscribe("/waypoint_manager/waypoint", 1, &map_changer_node::cb_wp, this);
+    reach_sub_ = nh_.subscribe("/waypoint_manager/waypoint/is_reached", 1, &map_changer_node::cb_reach, this);
+    next_wp_srv_ = nh_.serviceClient<std_srvs::Trigger>("/waypoint_manager/waypoint_server/next_waypoint");
     map_srv_ = nh_.serviceClient<nav_msgs::LoadMap>("/change_map");
     costmap_srv_ = nh_.serviceClient<nav_msgs::LoadMap>("/change_map_for_costmap");
     pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1, this);
@@ -20,39 +22,54 @@ void map_changer_node::cb_wp(const waypoint_manager_msgs::Waypoint::ConstPtr &ms
 {
     static int index = 0;
     static std::string old_id;
+    static bool tp_flag = false;
+    ros::Rate rate(0.2);
     if (config_list.size() - 1 >= index)
     {
         std::string id = config_list[index][0];
-        // if (msg->identity == id && msg->identity != old_id)
-        // {
-        //     send_map(index);
-        //     send_pose(msg->pose);
-        //     index++;
-        // }
-        if (msg->identity != id && id == old_id)
+        if (msg->identity == id && reach_flag && !tp_flag && msg->identity == old_id)
         {
+            rate.sleep();
+            call_next_wp();
+            tp_flag = true;
+        }else if (tp_flag)
+        {
+            send_emclpose(msg->pose);
             send_map(index);
-            send_pose(msg->pose);
+            send_initialpose(msg->pose);
+            tp_flag = false;
             index++;
         }
-    } else if (config_list.size() - 1 < index)
+    }else if (config_list.size() - 1 < index)
     {
         index = 0;
     }
     old_id = msg->identity;
 }
 
+void map_changer_node::cb_reach(std_msgs::Bool msg)
+{
+    reach_flag = msg.data;
+}
+
 void map_changer_node::read_yaml()
 {
-    pnh_.param("file_path", file_path_, std::string(ros::package::getPath("map_changer") += "/config/test1.yaml"));
-    ROS_INFO("Load %s", file_path_.c_str());
-    YAML::Node config = YAML::LoadFile(file_path_);
-    for (const auto& node:config["config"])
+    try
     {
-        std::array<std::string, 2> tmp;
-        tmp[0] = node["waypoint_id"].as<std::string>();
-        tmp[1] = node["map_file"].as<std::string>();
-        config_list.push_back(tmp);
+        pnh_.param("file_path", file_path_, std::string(ros::package::getPath("map_changer") += "/config/test1.yaml"));
+        ROS_INFO("Load %s", file_path_.c_str());
+        YAML::Node config = YAML::LoadFile(file_path_);
+        for (const auto& node:config["config"])
+        {
+            std::array<std::string, 2> tmp;
+            tmp[0] = node["waypoint_id"].as<std::string>();
+            tmp[1] = node["map_file"].as<std::string>();
+            config_list.push_back(tmp);
+        }   
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("%s", e.what());
     }
 }
 
@@ -61,26 +78,37 @@ void map_changer_node::send_map(int index)
     nav_msgs::LoadMap map, costmap;
     map.request.map_url = config_list[index][1] + ".yaml";
     costmap.request.map_url = config_list[index][1] + "_for_costmap.yaml";
-    if (map_srv_.call(map))
+    try
     {
+        map_srv_.call(map);
         ROS_INFO("Map change to %s", map.request.map_url.c_str());
-    } else {
-        ROS_ERROR("Failed to call service /change_map");
     }
-    if (costmap_srv_.call(costmap))
+    catch(const ros::Exception& e)
     {
+        ROS_ERROR("%s", e.what());
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("%s", e.what());
+    }
+    
+    try
+    {
+        map_srv_.call(costmap);
         ROS_INFO("Costmap change to %s", costmap.request.map_url.c_str());
-    } else {
-        ROS_ERROR("Failed to call service /change_map_for_costmap");
+    }
+    catch(const ros::Exception& e)
+    {
+        ROS_ERROR("%s", e.what());
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("%s", e.what());
     }
 }
 
-void map_changer_node::send_pose(geometry_msgs::Pose pose)
+void map_changer_node::send_emclpose(geometry_msgs::Pose pose)
 {
-    geometry_msgs::PoseWithCovarianceStamped msg;
-    msg.pose.pose = pose;
-    pose_pub_.publish(msg);
-
     double roll = 0, pitch = 0, yaw = 0;
     tf::Quaternion quat;
 	quaternionMsgToTF(pose.orientation, quat);
@@ -88,8 +116,32 @@ void map_changer_node::send_pose(geometry_msgs::Pose pose)
     nh_.setParam("/emcl2_node/initial_pose_x", pose.position.x);
     nh_.setParam("/emcl2_node/initial_pose_y", pose.position.y);
     nh_.setParam("/emcl2_node/initial_pose_y", yaw);
+}
 
+void map_changer_node::send_initialpose(geometry_msgs::Pose pose)
+{
+    geometry_msgs::PoseWithCovarianceStamped msg;
+    msg.pose.pose = pose;
+    pose_pub_.publish(msg);
     ROS_INFO("Teleport to x:%lf, y:%lf", pose.position.x, pose.position.y);
+}
+
+void map_changer_node::call_next_wp()
+{
+    std_srvs::Trigger srv;
+    try
+    {
+        next_wp_srv_.call(srv);
+        ROS_INFO("Call service : /next_waypoint");
+    }
+    catch(const ros::Exception& e)
+    {
+        ROS_ERROR("%s", e.what());
+    }
+    catch(const std::exception& e)
+    {
+        ROS_ERROR("%s", e.what());
+    }
 }
 }
 
